8423f1ba2d6f5e775cd14098e04fc8502282a9f2
Flowers Safety <40meta@gmail.com>
2022-12-06 13:48:00 +0600

    identifyFirstStartOfPacketMarker was generalized to identifyFirstMarker

--- 06d/src/TuningTrouble.hs
+++ 06d/src/TuningTrouble.hs
@@ -3,16 +3,19 @@ module TuningTrouble where
 import Data.List (nub)
 
 
-identifyFirstStartOfPacketMarker' ::  Int -> String -> (Int, String)
-identifyFirstStartOfPacketMarker' leftmostPosition (c1 : c2 : c3 : c4 : restBuffer) =
-    case length $ nub [c1, c2, c3, c4] of
-        4 -> (leftmostPosition, [c1, c2, c3, c4])
-        _ -> identifyFirstStartOfPacketMarker' (leftmostPosition + 1) (c2 : c3 : c4 : restBuffer)
-
-identifyFirstStartOfPacketMarker' leftmostPosition nonMatchedInput =
-    error $ "non matched input: " ++ nonMatchedInput ++ " at position " ++ show leftmostPosition
+identifyFirstMarker :: Int -> Int -> String -> (Int, String)
+identifyFirstMarker markerLength leftmostPosition buffer =
+    if length buffer < markerLength
+    then error $ "buffer is too short: " ++ buffer
+        ++ " at position " ++ show leftmostPosition
+        ++ " with marker length " ++ show markerLength
+    else let inspectedBufferPart = take markerLength buffer
+        in if (length $ nub inspectedBufferPart) == markerLength
+            then (leftmostPosition, inspectedBufferPart)
+            else identifyFirstMarker markerLength (leftmostPosition + 1) (drop 1 buffer)
+    
 
 identifyFirstStartOfPacketMarker :: String -> (Int, String)
-identifyFirstStartOfPacketMarker = identifyFirstStartOfPacketMarker' 4
+identifyFirstStartOfPacketMarker = identifyFirstMarker 4 4
 
 
