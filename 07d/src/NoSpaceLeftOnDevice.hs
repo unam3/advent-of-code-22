7bc6fc4445d0e02dd1c0b28b8237576c7d9946c0
Flowers Safety <40meta@gmail.com>
2022-12-08 01:29:45 +0600

    parsing functions WIP

--- 07d/src/NoSpaceLeftOnDevice.hs
+++ 07d/src/NoSpaceLeftOnDevice.hs
@@ -1,6 +1,32 @@
 module NoSpaceLeftOnDevice where
 
---parseInput :: String -> 
-parseInput = fmap id . lines
+
+type Name = String
+
+type Size = Int
+
+--data FSEntity = Dir Name DirContent | File Name Size
+
+--data FSEntity1 = Dir DirContent | File Int | NotYetFilled
+-- type AbsolutePath = [String]
+-- type FS = Map AbsolutePath FSEntity1
+
+data FSEntity = Dir Name | File Name Size
+
+type DirContent = [FSEntities]
+
+type ParsingState = (AbsolutePath, DirContent)
+
+-- currently parsing block:
+--  cd
+--  ls output
+
+--parse :: ParsingState -> String -> ParsingState
+parse (absolutePath, directoryContent) ('$':' ':'c':'d':' ':path) = 
+parse (absolutePath, directoryContent) ('d':'i':'r':' ':name) = dirName
+parse (absolutePath, directoryContent) fileString = words -- give us [lengthS, name]
+
+--parseInput :: String -> FSEntity
+parseInput = parse . filter (/= "$ ls"). lines
 
 
