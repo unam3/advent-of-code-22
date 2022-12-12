dc7a7c1f64349719211485028f3bf21d659f0cdb
Flowers Safety <40meta@gmail.com>
2022-12-12 20:04:39 +0600

    parseInput was implemented

--- 09d/src/RopeBridge.hs
+++ 09d/src/RopeBridge.hs
@@ -3,8 +3,20 @@ module RopeBridge where
 data Motion = R Int | U Int | L Int | D Int
     deriving (Eq, Show)
 
+parseMotion :: String -> Motion
+parseMotion (direction : ' ' : number) =
+    case direction of
+        'R' -> R $ read number
+        'U' -> U $ read number
+        'L' -> L $ read number
+        'D' -> D $ read number
+        undefinedDirection -> error $ "wrong motion direction: " ++ show undefinedDirection
+
+parseMotion wrongInput = error $ "wrong input for parseMotion: " ++ wrongInput
+
+
 parseInput :: String -> [Motion] 
-parseInput = undefined
+parseInput = fmap parseMotion . lines
 
 
 type Coords = (Int, Int)
--- 09d/test/RopeBridgeSpec.hs
+++ 09d/test/RopeBridgeSpec.hs
@@ -14,7 +14,7 @@ spec = do
         it "works"
             $ shouldBe
                 (parseInput testInput)
-                "42"
+                [R 4,U 4,L 3,D 1,R 4,D 1,L 5,R 2]
 
     describe "f" $ do
         it "works"
