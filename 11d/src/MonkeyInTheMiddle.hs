996885efd6e2ca567ecc44a882dcbf6ec38cf47d
Flowers Safety <40meta@gmail.com>
2022-12-27 16:33:50 +0600

    inspectItem implementation WIP

--- 11d/src/MonkeyInTheMiddle.hs
+++ 11d/src/MonkeyInTheMiddle.hs
@@ -2,7 +2,7 @@ module MonkeyInTheMiddle where
 
 
 import Data.List (isPrefixOf, foldl', stripPrefix)
-import Data.Vector (Vector, fromList)
+import Data.Vector (Vector, (//), fromList)
 import Text.Read (readMaybe)
 
 type Round = Int
@@ -12,7 +12,7 @@ type InspectedItems = Vector Int
 type State = (Round, InspectedItems)
 
 
-type StartingItems = [Int]
+type ItemWorryLevel = Int
 
 data Operation = MultiplyBy Int | Add Int | Sqr
     deriving (Eq, Show)
@@ -25,7 +25,9 @@ type ThrowIfFalseTo = Int
 
 type DivideThenThrow = (DivisibleBy, ThrowIfTrueTo, ThrowIfFalseTo)
 
-type InputData = Vector (StartingItems, Operation, DivideThenThrow)
+type MonkeyState = ([ItemWorryLevel], Operation, DivideThenThrow)
+
+type InputData = Vector MonkeyState
 
 -- from ../05d/src/SupplyStacks
 group :: [[String]] -> String -> [[String]]
@@ -38,7 +40,7 @@ splitByEmptyLines :: [String] -> [[String]]
 splitByEmptyLines = reverse . fmap reverse . foldl' group [[]]
 
 
-parseMonkeySection :: [String] -> (StartingItems, Operation, DivideThenThrow)
+parseMonkeySection :: [String] -> ([ItemWorryLevel], Operation, DivideThenThrow)
 parseMonkeySection [_, startingItemsS, operationS, divisibleByS, throwIfTrueToS, throwIfFalseToS] =
 
     let --(Just monkeyNumber) =
@@ -48,7 +50,7 @@ parseMonkeySection [_, startingItemsS, operationS, divisibleByS, throwIfTrueToS,
         --        >>= readMaybe
 
         -- Starting items: 62, 68, 56, 65, 94, 78
-        (Just startingItems) =
+        (Just startingItemWorryLevels) =
             stripPrefix "  Starting items: " startingItemsS
                 >>= Just . (\ numbers -> "[" ++ numbers ++ "]")
                 >>= readMaybe
@@ -77,7 +79,7 @@ parseMonkeySection [_, startingItemsS, operationS, divisibleByS, throwIfTrueToS,
                 >>= readMaybe
 
     --in (monkeyNumber, startingItems, operation, (divisibleBy, throwIfTrueTo, throwIfFalseTo))
-    in (startingItems, operation, (divisibleBy, throwIfTrueTo, throwIfFalseTo))
+    in (startingItemWorryLevels, operation, (divisibleBy, throwIfTrueTo, throwIfFalseTo))
 
 parseMonkeySection nonMatched = error $ "non matched input list: " ++ show nonMatched
 
@@ -85,4 +87,45 @@ parseMonkeySection nonMatched = error $ "non matched input list: " ++ show nonMa
 parseInput :: String -> InputData
 parseInput = fromList . fmap parseMonkeySection . splitByEmptyLines . lines
 
+
+modify :: ItemWorryLevel -> Operation -> ItemWorryLevel
+modify itemWorryLevel Sqr = itemWorryLevel * itemWorryLevel
+modify itemWorryLevel (Add n) = n + itemWorryLevel
+modify itemWorryLevel (MultiplyBy n) = n * itemWorryLevel
+
+
+throw :: InputData -> ItemWorryLevel -> Int -> InputData
+throw inputData itemWorryLevel monkeyToThrow = undefined 
+
+inspectItem :: MonkeyState -> InputData -> Int -> InputData
+inspectItem (itemWorryLevels, operation, (divisibleBy, throwIfTrueTo, throwIfFalseTo)) inputData itemWorryLevel =
+    {-
+        Monkey inspects an item with a worry level of 79.
+        Worry level is multiplied by 19 to 1501.
+        Monkey gets bored with item. Worry level is divided by 3 to 500.
+        Current worry level is not divisible by 23.
+        Item with worry level 500 is thrown to monkey 3.
+    -}
+    let worryLevel' = modify itemWorryLevel operation
+        worryLevelAfterGetBored = round ((fromInteger $ toInteger worryLevel') / 3)
+        isDivisibleByTestNumber = (rem worryLevelAfterGetBored divisibleBy) == 0
+
+        -- empty the list for current monky
+        -- need monkey index!
+        inputDataWithCurrentMunkeyEmptyItemsList = undefined -- (//) inputData [(, [])]
+
+    in if isDivisibleByTestNumber
+
+        -- add item with worryLevelAfterGetBored to monkey from throwIfTrueTo/throwIfFalseTo
+        then undefined --- throw to monkey number throwIfTrueTo
+
+        else undefined --- throw to monkey number throwIfFalseTo
+
+
+
+inspect :: InputData -> MonkeyState -> InputData
+inspect inputData state@(itemWorryLevels, _, _) =
+    let pluh = foldl' (inspectItem state) inputData itemWorryLevels
+    in undefined
+    
 
