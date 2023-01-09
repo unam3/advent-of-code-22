5e98ea18f0639e8ebeaee3a557d3d21b45b16cfc
Flowers Safety <40meta@gmail.com>
2023-01-09 13:46:20 +0600

    getting ready to write for each item separate file

--- 11d/app/Main.hs
+++ 11d/app/Main.hs
@@ -1,6 +1,32 @@
 module Main where
 
+
+import Control.Monad (foldM_)
+
 import MonkeyInTheMiddle
+
 
 main :: IO ()
 main = readFile "testInput" >>= print . getMonkeyBusinessLevel . runNRounds id 99 . parseInput
+
+
+addSuffixIfNeeded :: [String] -> String -> ([String], String)
+addSuffixIfNeeded alreadyProcessed string =
+    if elem string alreadyProcessed
+    then addSuffixIfNeeded alreadyProcessed (string ++ "_suffix")
+    else (alreadyProcessed, string)
+
+f :: IO ()
+f = foldM_
+    (\ acc el ->
+
+        do print $ "initial string: " ++ el
+            
+           let (acc', el') = addSuffixIfNeeded acc el
+
+           print $ "probably modified one: " ++ el'
+
+           pure acc'
+    )
+    ["b"]
+    ["a", "b", "c"]
