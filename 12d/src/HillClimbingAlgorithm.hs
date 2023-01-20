f2e7e7119735ff2a87b9cdddc206d85ec54311a8
Flowers Safety <40meta@gmail.com>
2023-01-20 17:35:42 +0600

    parseInput was implemented

--- 12d/src/HillClimbingAlgorithm.hs
+++ 12d/src/HillClimbingAlgorithm.hs
@@ -1,6 +1,37 @@
 module HillClimbingAlgorithm where
 
---parseInput :: String -> 
-parseInput = fmap id . lines
 
+import Data.HashMap.Strict (HashMap, fromList)
+
+
+type X = Int
+type Y = Int
+
+type SquareCoords = (X, Y)
+
+type Heightmap = HashMap SquareCoords Char
+
+
+assignX :: [Char] -> [(Int, Char)]
+assignX = zip [0..]
+
+
+assignY :: [[(Int, Char)]] -> [[(SquareCoords, Char)]]
+assignY =
+    zipWith
+        (\ y rowXChar ->
+            fmap
+                (\ (x, char) -> ((x, y), char))
+                rowXChar
+        )
+        [0..]
+
+assignCoords :: [Char] -> [(SquareCoords, Char)]
+assignCoords = concat . assignY . fmap assignX . lines
+
+parseInput :: String -> Heightmap
+parseInput = fromList . assignCoords
+
+
+type Path = [SquareCoords]
 
