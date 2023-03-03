80dd4feca21a60826fdc349e02abb48b065f93ee
Flowers Safety <40meta@gmail.com>
2023-03-03 18:07:02 +0600

    now ff will write all files
    cat 13 17 19 23|sort|uniq -d -c|less

--- 11d/src/Experiments.hs
+++ 11d/src/Experiments.hs
@@ -2,15 +2,26 @@ module Experiments where
 
 
 import Data.List (foldl', intersperse)
+import Control.Applicative (liftA2)
 import Control.Monad (void)
 
 
 getX200 :: Int -> [Int]
-getX200 n = foldl' (\ acc el -> acc ++ [el * n]) [] [1..200]
+getX200 n = foldl' (\ acc el -> acc ++ [el * n]) [] [1..1000]
 
-f :: Int -> IO ()
-f n = writeFile (show n)  (concat . intersperse "\n" . fmap show $ getX200 n)
+f :: Int -> IO String
+f n =
 
-ff :: [Int] -> IO ()
-ff = foldl' (\ _ n -> void $ f n) (pure ())
+    do
 
+    let list = concat . intersperse "\n" . fmap show $ getX200 n
+   
+    writeFile (show n) list
+
+    pure list
+
+
+ff :: [Int] -> IO [String]
+ff = foldl' (\ acc n -> liftA2 (:) (f n) acc) (pure [])
+
+-- void $ ff [13,17,19,23]
